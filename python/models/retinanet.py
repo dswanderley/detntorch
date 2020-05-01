90d2a58a3857551c00f2e065e20781c21ed3233d
Diego Santos Wanderley <dswanderley@gmail.com>
2020-05-01 10:26:14 +0100

    ADD: RegressionModel a subnetwork responsible for performing bounding box regression using the backbone’s output.

--- python/models/retinanet.py
+++ python/models/retinanet.py
@@ -15,6 +15,40 @@ except:
     from fpn import PyramidFeatures
 
 
+
+class RegressionModel(nn.Module):
+    '''
+    RegressionModel net: a subnetwork responsible for performing
+        bounding box regression using the backbone’s output.
+    '''
+    def __init__(self, in_features, num_features=255, num_anchors=9):
+        super(RegressionModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_features,  num_features, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(num_features, num_features, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(num_features, num_features, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(num_features, num_features, kernel_size=3, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv5 = nn.Conv2d(feature_size, num_anchors * 4, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        # Box subnet
+        out = self.conv1(x)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.relu(out)
+        out = self.conv4(out)
+        out = self.relu(out)
+        # Output conv
+        out = self.conv5(out)
+        # out is B x C x W x H, with C = 4*num_anchors
+        out = out.permute(0, 2, 3, 1)
+
+        return out.contiguous().view(out.shape[0], -1, 4)
+
+
 class RetinaNet(nn.Module):
     num_anchors = 9
 
