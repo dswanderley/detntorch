a3dd3f51aa8aabee1b079e3f0c215c561e1bd12c
Diego Wanderley <dswanderley@gmail.com>
2020-05-12 12:44:55 +0100

    CHANGE: Train batch order.

--- python/train.bat
+++ python/train.bat
@@ -1,10 +1,10 @@
 call conda activate torch37
 
+python train_rcnn.py --batch_size 8
+python train_retina.py --batch_size 8
 python train_yolo.py --model_name yolov3 --batch_size 6
 python train_yolo.py --model_name yolov3_fol --batch_size 6
 python train_yolo.py --model_name yolov3-tiny --batch_size 8
 python train_yolo.py --model_name yolov3-tiny_fol --batch_size 8
-python train_rcnn.py --batch_size 8
-python train_retina.py --batch_size 8
 
 pause
