224e2b5dba8b4640afcb3ff22721cd6e2998a325
Diego Wanderley <dswanderley@gmail.com>
2020-05-13 19:46:47 +0100

    ADD: RetinaNet on detect batch script.

--- python/detect.bat
+++ python/detect.bat
@@ -1,10 +1,10 @@
 call conda activate torch37
 
-python detect_yolo.py --model_name yolov3 --weights_path ../weights/20200419_1419_yolov3_weights.pth.tar
-python detect_yolo.py --model_name yolov3-tiny --weights_path ../weights/20200419_1509_yolov3-tiny_weights.pth.tar
-python detect_yolo.py --model_name yolov3-tiny_fol --weights_path ../weights/20200419_1522_yolov3-tiny_fol_weights.pth.tar
-python detect_yolo.py --model_name yolov3-tiny --weights_path ../weights/20200419_1636_yolov3-tiny_weights.pth.tar
-python detect_yolo.py --model_name yolov3-tiny_fol --weights_path ../weights/20200419_1648_yolov3-tiny_fol_weights.pth.tar
-python detect_yolo.py --model_name yolov3_fol --weights_path ../weights/20200419_1701_yolov3_fol_weights.pth.tar
-python detect_rcnn.py --weights_path ../weights/20200419_1604_faster_rcnn_weights.pth.tar
+python detect_rcnn.py --weights_path ../weights/20200512_1958_faster_rcnn_weights.pth.tar
+python detect_retina.py --weights_path ../weights/20200512_2035_retinanet_weights.pth.tar
+python detect_yolo.py --model_name yolov3 --weights_path ../weights/20200512_2122_yolov3_weights.pth.tar
+python detect_yolo.py --model_name yolov3_fol --weights_path ../weights/20200512_2152_yolov3_fol_weights.pth.tar
+python detect_yolo.py --model_name yolov3-tiny --weights_path ../weights/20200512_2223_yolov3-tiny_weights.pth.tar
+python detect_yolo.py --model_name yolov3-tiny_fol --weights_path ../weights/20200512_2236_yolov3-tiny_fol_weights.pth.tar
+
 pause
