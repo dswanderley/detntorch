90b0d862956326f5462e258051ce69277873ac43
Diego Wanderley <dswanderley@gmail.com>
2020-05-13 22:16:53 +0100

    CHANGE: Use all tags in all dictionaries in the saved weights folder.

--- python/read_weights.py
+++ python/read_weights.py
@@ -8,59 +8,43 @@ Created on Sun Mar 29 18:31:10 2020
 
 import os
 import csv
+import copy
 import torch
 import argparse
+import collections
 
 #from models.rcnn import FasterRCNN
 #from models.yolo import Darknet
 
 
-def read_state_dict(state):
+def read_state_dict(state, _dict):
 
-    header = [  'epoch',
-                'batch_size',
-                'optimizer',
-                'best_loss_train',
-                'best_ap_val',
-                'val_precision',
-                'val_recall',
-                'val_mAP',
-                'val_f1',
-                'device'  ]
-    row  = [ '', '', '', '', '','', '', '', '', '' ]
-
+    state_dict = copy.deepcopy(_dict)
     # Read state dict
     for key, value in state.items():
-        # Epoch of saving
-        if key == 'epoch':
-            row[0] = value
-        if key == 'batch_size':
-            row[1] = value
-        # Optimizer used
-        if key == 'optimizer':
-            row[2] = value.split(" ")[0]
-        # Train loss score these weights
-        if key == 'best_loss_train':
-            row[3] = value
-        # Validation score for these weights
-        if key == 'best_ap_val':
-            row[4] = value
-        if key == 'val_precision':
-            row[5] = value
-        # Validation score for these weights
-        if key == 'val_recall':
-            row[6] = value
-        # Validation score for these weights
-        if key == 'val_mAP':
-            row[7] = value
-        # Validation score for these weights
-        if key == 'val_f1':
-            row[8] = value
-        # Device used during training
-        if key == 'device':
-            row[9] = value
+        # Check if exist and assigns
+        if key in state_dict:
+            if key == 'optimizer':
+                value = value.split(" ")[0]
+            state_dict[key] = value
+       
+    return state_dict
 
-    return header, row
+
+def get_dict(states):
+    '''
+    Create a dictionary with all tags from a list of states dictionary.
+    '''
+    key_list = ["model", "date", "time"]
+    for state in states:
+        # Read state dict
+        for key, val in state.items():
+            if (key not in key_list) and (type(val) is not dict and type(val) is not collections.OrderedDict): 
+                key_list.append(key)
+    # Convert list to dict
+    res_dct = {item: None for item in key_list}
+
+    return res_dct
 
 
 if __name__ == "__main__":
@@ -77,35 +61,38 @@ if __name__ == "__main__":
         for r, d, f in os.walk(opt.folder):
             [ (flist.append(os.path.join(r, x)), fnames.append(x)) for x in f if '.tar' in x ]
 
-        table = []
-        for wpath, wname in zip(flist,fnames):
+        dict_table = []
+        
+        states = [ torch.load(wpath) for wpath in flist ]
+        states_dict = get_dict(states)
+        keys = states_dict.keys()
+
+        for wname, state in zip(fnames, states):
             # split weights name
             name_splited = wname.split("_weights.")[0].split("_")
             train_date = name_splited[0]
             train_time = name_splited[1]
             model_name = '_'.join(name_splited[2:])
+
             # Read weights state dict
-            state = torch.load(wpath)
-            header, row = read_state_dict(state)
+            row = read_state_dict(state, states_dict)
+            row['model'] = model_name
+            row['date'] = train_date
+            row['time'] = train_time
 
-            if len(table) == 0:
-                table_header = [ "model", "date", "time" ] + header
-                table.append(table_header)
-                print( table_header )
-
-            table_row = [ model_name, int(train_date), int(train_time) ] + row
-            table.append(table_row)
-            print( table_row )
+            dict_table.append(row)
 
         # Save data on a csv
         with open(opt.folder + "/state.csv", 'w', newline='') as fp:
-            writer = csv.writer(fp, delimiter=';')
-            writer.writerows(table)
+            dict_writer = csv.DictWriter(fp, keys, delimiter=';')
+            dict_writer.writeheader()
+            dict_writer.writerows(dict_table)
+            #writer = csv.writer(fp, delimiter=';')
+            #writer.writerows(table)
 
     else:
         # Load state dictionary
         state = torch.load(opt.weights_path)
-        #model.load_state_dict(state['state_dict'])
         header, row = read_state_dict(state)
         print(header)
         print(row)
