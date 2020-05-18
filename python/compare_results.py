baa05f85c26d2446e85c3e8bfa71b8f937ba142b
Diego Wanderley <dswanderley@gmail.com>
2020-05-18 20:02:38 +0100

    ADD: Compute precision, recall, AP and f1.

--- python/compare_results.py
+++ python/compare_results.py
@@ -1,14 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 17:51:01 2019
+@author: Diego Wanderley
+@python: 3.7
+@description: Compare all models predictions with groud truth dataset.
+"""
+
 import os
 import csv
 import copy
 import torch
+import numpy as np
+from models.yolo_utils.utils import bbox_iou, ap_per_class
 
-from models.yolo_utils.utils import bbox_iou
+# Classes names
+class_names = ['background','follicle','ovary']
 
-
+# Files paths
 dataset_file = '../datasets/ovarian/data.csv'
 predict_path = '../predictions'
 result_file = 'results.csv'
+
 
 prediction_files = [ os.path.join(r,result_file)  for r, d, f in os.walk(predict_path) if result_file in f ]
 
@@ -44,10 +56,21 @@ for pfile in prediction_files:
         detections = [ { k: v for k, v in row.items() }
             for row in csv.DictReader(f, delimiter=';') ]
     lbl_key = 'cls_pred' if 'cls_pred' in detections[0] else 'labels'
+
     # Convert list of detection to a list of dictionary by filename
     [ predictions[p_data['fname']].append(p_data) for p_data in detections if p_data['fname'] in testset_names ]
     comparison_items = []
     comparison_ovral = []
+
+    # Set of data for all images evaluates with this model
+    model_true_positives = []
+    model_pred_scores = []
+    model_pred_labels = []
+    model_tgt_labels = []
+    model_num_tgt = []
+    model_num_dtn = []
+    model_num_fn = []
+    model_num_duplicate = []
     
     # Iterate filenames
     for fname in testset_names:
@@ -57,10 +80,15 @@ for pfile in prediction_files:
         # Temp storages
         iou_list = []
         gt_id_list = []
-        
+        pred_tp_list = []
+        pred_scores_list = []
+        pred_lbls_list = []
+        gt_lbls_list = [ class_names.index(gt['class']) for gt in gtruth ]
+
         # Iterate image detections 
         for d in range(len(detect)):
             dtn = detect[d]
+            score = dtn['scores'] if 'scores' in dtn else dtn['cls_conf']
             box_dt = torch.tensor([ float(dtn['x1']), float(dtn['y1']), float(dtn['x2']), float(dtn['y2']) ])
             best_iou = 0
             best_gt_id = -1
@@ -83,12 +111,22 @@ for pfile in prediction_files:
             dtn['gt_idx'] = best_gt_id
             if best_gt_id < 0:
                 dtn['status'] = 'FP'
+                pred_tp_list.append(0)
             else:
                 dtn['status'] = 'TP'
+                pred_tp_list.append(1)
             # Append to be compared
             gt_id_list.append(best_gt_id)
             iou_list.append(best_iou)
-
+            pred_scores_list.append(float(score))
+            pred_lbls_list.append(class_names.index(dtn[lbl_key]))
+            
+        # Image prediction evaluation
+        precision, recall, AP, f1, ap_class = ap_per_class( np.array(pred_tp_list), 
+                                                            np.array(pred_scores_list), 
+                                                            np.array(pred_lbls_list), 
+                                                            gt_lbls_list )
+    
         # Check for duplicated (when NMS fail)
         duplicate = []
         for i in range(len(gt_id_list)-1):
@@ -102,7 +140,12 @@ for pfile in prediction_files:
         false_negative = [ gt_i for gt_i in range(len(gtruth)) if gt_i not in gt_id_list ]
         false_positive = [ dt_i for dt_i in range(len(detect)) if gt_id_list[dt_i] < 0 ]
         true_positive =  [ gt_i for gt_i in range(len(gtruth)) if gt_i in list(set(gt_id_list)) ]
-                
+        # Append to count
+        model_num_tgt.append(len(gtruth))
+        model_num_dtn.append(len(detect))
+        model_num_fn.append(len(false_negative))
+        model_num_duplicate.append(len(duplicate))
+
         # Dictionary with general data
         overall_dict = {
             'filename': fname,
@@ -115,12 +158,51 @@ for pfile in prediction_files:
             'fn': false_negative,
             'fp': false_positive,
             'tp': true_positive,
-            'duplicates': duplicate
+            'duplicates': duplicate,
+            'precision_mean':precision.mean(),
+            'recall_mean':recall.mean(),
+            'ap_mean':AP.mean(),
+            'f1_mean':f1.mean()
         }
+        for i in range(len(ap_class)):
+            overall_dict['precision_' + str(i+1)] = precision[i]
+            overall_dict['recall_' + str(i+1)] = recall[i]
+            overall_dict['ap_' + str(i+1)] = AP[i]
+            overall_dict['f1_' + str(i+1)] = f1[i]
 
         # Store on a list with all detections of all files
         comparison_items += detect
         comparison_ovral.append(overall_dict)
+        # Add to mode evaluation list
+        model_true_positives += pred_tp_list
+        model_pred_scores    += pred_scores_list
+        model_pred_labels    += pred_lbls_list
+        model_tgt_labels     += gt_lbls_list
+
+    # Evaluate model
+    precision, recall, AP, f1, ap_class = ap_per_class( np.array(model_true_positives), 
+                                                        np.array(model_pred_scores), 
+                                                        np.array(model_pred_labels), 
+                                                        model_tgt_labels )
+    overall_dict = {
+        'filename': 'model',
+        'num_gt': sum(model_num_tgt),
+        'num_detn': sum(model_num_dtn),
+        'num_fn': sum(model_num_fn),
+        'num_fp':  model_true_positives.count(0),
+        'num_tp': model_true_positives.count(1),
+        'num_duplicates': sum(model_num_duplicate),
+        'precision_mean':precision.mean(),
+        'recall_mean':recall.mean(),
+        'ap_mean':AP.mean(),
+        'f1_mean':f1.mean()
+    }
+    for i in range(len(ap_class)):
+        overall_dict['precision_' + str(i+1)] = precision[i]
+        overall_dict['recall_' + str(i+1)] = recall[i]
+        overall_dict['ap_' + str(i+1)] = AP[i]
+        overall_dict['f1_' + str(i+1)] = f1[i]
+    comparison_ovral.append(overall_dict)
 
     # Save comparisons
     keys_i = comparison_items[0].keys()
