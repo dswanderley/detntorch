ad1c64de78fdd4f4543414b36b854699454129b5
Diego Wanderley <dswanderley@gmail.com>
2020-05-20 19:42:07 +0100

    CHANGE: Auxiliary classes of retinanet from original github code.

--- python/models/retina_utils/anchors.py
+++ python/models/retina_utils/anchors.py
@@ -31,8 +31,6 @@ class Anchors(nn.Module):
 
     def forward(self, image):
         
-        device = torch.device("cuda:0" if torch.cuda.is_available() and image.device.type == 'cuda'  else "cpu")
-
         image_shape = image.shape[2:]
         image_shape = np.array(image_shape)
         image_shapes = [(image_shape + 2 ** x - 1) // (2 ** x) for x in self.pyramid_levels]
@@ -47,7 +45,10 @@ class Anchors(nn.Module):
 
         all_anchors = np.expand_dims(all_anchors, axis=0)
 
-        return torch.from_numpy(all_anchors.astype(np.float32)).to(device)
+        if torch.cuda.is_available():
+            return torch.from_numpy(all_anchors.astype(np.float32)).cuda()
+        else:
+            return torch.from_numpy(all_anchors.astype(np.float32))
 
 
 def generate_anchors(base_size=16, ratios=None, scales=None):
@@ -96,8 +97,16 @@ def compute_shape(image_shape, pyramid_levels):
     return image_shapes
 
 
-def anchors_for_shape(image_shape, pyramid_levels=None, ratios=None,
-                    scales=None,strides=None,sizes=None,shapes_callback=None):
+def anchors_for_shape(
+    image_shape,
+    pyramid_levels=None,
+    ratios=None,
+    scales=None,
+    strides=None,
+    sizes=None,
+    shapes_callback=None,
+):
+
     image_shapes = compute_shape(image_shape, pyramid_levels)
 
     # compute anchors over all pyramid levels
@@ -133,27 +142,49 @@ def shift(shape, stride, anchors):
     return all_anchors
 
 
+    shift_x = (np.arange(0, shape[1]) + 0.5) * stride
+    shift_y = (np.arange(0, shape[0]) + 0.5) * stride
+
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+
+    shifts = np.vstack((
+        shift_x.ravel(), shift_y.ravel(),
+        shift_x.ravel(), shift_y.ravel()
+    )).transpose()
+
+    # add A anchors (1, A, 4) to
+    # cell K shifts (K, 1, 4) to get
+    # shift anchors (K, A, 4)
+    # reshape to (K*A, 4) shifted anchors
+    A = anchors.shape[0]
+    K = shifts.shape[0]
+    all_anchors = (anchors.reshape((1, A, 4)) + shifts.reshape((1, K, 4)).transpose((1, 0, 2)))
+    all_anchors = all_anchors.reshape((K * A, 4))
+
+    return all_anchors
+
+
 class BBoxTransform(nn.Module):
-    '''
-    Layer for applying regression values to boxes.
-    '''
+
     def __init__(self, mean=None, std=None):
         super(BBoxTransform, self).__init__()
-        # init mean
         if mean is None:
-            self.mean = torch.from_numpy(np.array([0, 0, 0, 0]).astype(np.float32))
+            if torch.cuda.is_available():
+                self.mean = torch.from_numpy(np.array([0, 0, 0, 0]).astype(np.float32)).cuda()
+            else:
+                self.mean = torch.from_numpy(np.array([0, 0, 0, 0]).astype(np.float32))
+
         else:
             self.mean = mean
-        # init std
         if std is None:
-           self.std = torch.from_numpy(np.array([0.1, 0.1, 0.2, 0.2]).astype(np.float32))
+            if torch.cuda.is_available():
+                self.std = torch.from_numpy(np.array([0.1, 0.1, 0.2, 0.2]).astype(np.float32)).cuda()
+            else:
+                self.std = torch.from_numpy(np.array([0.1, 0.1, 0.2, 0.2]).astype(np.float32))
         else:
             self.std = std
 
     def forward(self, boxes, deltas):
-        device = torch.device("cuda:0" if torch.cuda.is_available() and boxes.device.type == 'cuda'  else "cpu")
-        self.mean.to(device)
-        self.std.to(device)
 
         widths  = boxes[:, :, 2] - boxes[:, :, 0]
         heights = boxes[:, :, 3] - boxes[:, :, 1]
@@ -181,9 +212,7 @@ class BBoxTransform(nn.Module):
 
 
 class ClipBoxes(nn.Module):
-    '''
-    Layer to clip box values to lie inside a given shape.
-    '''
+
     def __init__(self, width=None, height=None):
         super(ClipBoxes, self).__init__()
 
