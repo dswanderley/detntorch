a5f49b1391b5db57d2e60a1b1fde3f3f20b30336
Diego Wanderley <dswanderley@gmail.com>
2020-06-06 15:36:47 +0100

    CHANGE: Set number of clusters and include ovary.

--- python/clusters.py
+++ python/clusters.py
@@ -6,8 +6,8 @@ from utils.datasets import OvaryDataset
 from torch.utils.data import DataLoader
 
 
-N_CLUSTERS = 9
-ovary = False
+N_CLUSTERS = 6
+ovary = True
 
 colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
