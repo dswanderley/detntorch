2d732e92e382e5dad179ff312cec972deac9979d
Diego Wanderley <dswanderley@gmail.com>
2020-06-07 12:12:13 +0100

    CHANGE: Detect  script for three classes.

--- python/detect.bat
+++ python/detect.bat
@@ -1,19 +1,23 @@
 call conda activate torch37
 
-python detect_rcnn.py   --weights_path ../weights/20200512_1958_faster_rcnn_weights.pth.tar
-python detect_yolo.py   --model_name yolov3 --weights_path ../weights/20200512_2122_yolov3_weights.pth.tar
-python detect_yolo.py   --model_name yolov3_fol --weights_path ../weights/20200512_2152_yolov3_fol_weights.pth.tar
-python detect_yolo.py   --model_name yolov3-tiny --weights_path ../weights/20200512_2223_yolov3-tiny_weights.pth.tar
-python detect_yolo.py   --model_name yolov3-tiny_fol --weights_path ../weights/20200512_2236_yolov3-tiny_fol_weights.pth.tar
-python detect_retina.py --backbone resnet18 --weights_path ../weights/20200531_1631_retinanet_resnet18_weights.pth.tar      --score_thres 0.4 --apply_nms True
-python detect_retina.py --backbone resnet18 --weights_path ../weights/20200531_1631_retinanet_resnet18_weights.pth.tar      --score_thres 0.4
-python detect_retina.py --backbone resnet34 --weights_path ../weights/20200531_1659_retinanet_resnet34_weights.pth.tar      --score_thres 0.4 --apply_nms True
-python detect_retina.py --backbone resnet34 --weights_path ../weights/20200531_1659_retinanet_resnet34_weights.pth.tar      --score_thres 0.4
-python detect_retina.py --backbone resnet50 --weights_path ../weights/20200531_1728_retinanet_resnet50_weights.pth.tar      --score_thres 0.4 --apply_nms True
-python detect_retina.py --backbone resnet50 --weights_path ../weights/20200531_1728_retinanet_resnet50_weights.pth.tar      --score_thres 0.4
-python detect_retina.py --backbone resnext50 --weights_path ../weights/20200531_1852_retinanet_resnext50_weights.pth.tar --score_thres 0.4 --apply_nms True
-python detect_retina.py --backbone resnext50 --weights_path ../weights/20200531_1852_retinanet_resnext50_weights.pth.tar --score_thres 0.4 
-python detect_retina.py --backbone resnext101 --weights_path ../weights/20200531_1932_retinanet_resnext101_weights.pth.tar --score_thres 0.4 --apply_nms True
-python detect_retina.py --backbone resnext101 --weights_path ../weights/20200531_1932_retinanet_resnext101_weights.pth.tar --score_thres 0.4
+python detect_rcnn.py --num_classes 3   --weights_path ../weights/20200606_1715_faster_rcnn_weights.pth.tar
+
+python detect_yolo.py --model_name yolov3      --num_anchors 9 --num_classes 3 --weights_path ../weights/20200607_0918_yolov3_a9_c3_weights.pth.tar
+python detect_yolo.py --model_name yolov3      --num_anchors 6 --num_classes 3 --weights_path ../weights/20200606_2252_yolov3_a6_c3_weights.pth.tar
+python detect_yolo.py --model_name yolov3-tiny --num_anchors 6 --num_classes 3 --weights_path ../weights/20200606_2326_yolov3-tiny_a6_c3_weights.pth.tar
+python detect_yolo.py --model_name yolov3-tiny --num_anchors 4 --num_classes 3 --weights_path ../weights/20200607_0953_yolov3-tiny_a4_c3_weights.pth.tar
+
+python detect_retina.py --backbone resnet18   --num_classes 3 --weights_path ../weights/20200606_1759_retinanet_resnet18_weights.pth.tar   --score_thres 0.4 --apply_nms True
+python detect_retina.py --backbone resnet18   --num_classes 3 --weights_path ../weights/20200606_1759_retinanet_resnet18_weights.pth.tar   --score_thres 0.4
+python detect_retina.py --backbone resnet34   --num_classes 3 --weights_path ../weights/20200606_1827_retinanet_resnet34_weights.pth.tar   --score_thres 0.4 --apply_nms True
+python detect_retina.py --backbone resnet34   --num_classes 3 --weights_path ../weights/20200606_1827_retinanet_resnet34_weights.pth.tar   --score_thres 0.4
+python detect_retina.py --backbone resnet50   --num_classes 3 --weights_path ../weights/20200606_1901_retinanet_resnet50_weights.pth.tar   --score_thres 0.4 --apply_nms True
+python detect_retina.py --backbone resnet50   --num_classes 3 --weights_path ../weights/20200606_1901_retinanet_resnet50_weights.pth.tar   --score_thres 0.4
+python detect_retina.py --backbone resnet101  --num_classes 3 --weights_path ../weights/20200606_1939_retinanet_resnet101_weights.pth.tar  --score_thres 0.4 --apply_nms True
+python detect_retina.py --backbone resnet101  --num_classes 3 --weights_path ../weights/20200606_1939_retinanet_resnet101_weights.pth.tar  --score_thres 0.4
+python detect_retina.py --backbone resnext50  --num_classes 3 --weights_path ../weights/20200606_2044_retinanet_resnext50_weights.pth.tar  --score_thres 0.4 --apply_nms True
+python detect_retina.py --backbone resnext50  --num_classes 3 --weights_path ../weights/20200606_2044_retinanet_resnext50_weights.pth.tar  --score_thres 0.4 
+python detect_retina.py --backbone resnext101 --num_classes 3 --weights_path ../weights/20200606_2125_retinanet_resnext101_weights.pth.tar --score_thres 0.4 --apply_nms True
+python detect_retina.py --backbone resnext101 --num_classes 3 --weights_path ../weights/20200606_2125_retinanet_resnext101_weights.pth.tar --score_thres 0.4
 
 pause
